09cf73d2080626ec192b677e23a6789762309763
dkmtech32 <109049675+dkmtech32@users.noreply.github.com>
2023-06-01 15:46:20 +0700

    install materialize

---
+++ src/components/Testmaterialize.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Testmaterialize() {
+  return (
+    <body>
+      <div class="container-fluid">
+        <p>ADSDSDD</p>
+      </div>
+    </body> 
+  )
+}
