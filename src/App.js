eea6f158686857c7c17841ae335314e37ddd17f8
dkmtech32 <109049675+dkmtech32@users.noreply.github.com>
2023-06-01 15:18:20 +0700

    merge css files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 
 import './App.css';
 import Navigation from './components/Navigation';
-import './Nav.css';
-import './Films.css';
 import Footer from './components/Footer';
-import './components/Footer.css';
 import '@fortawesome/react-fontawesome';
 import '@fortawesome/free-solid-svg-icons';
 import Main from './components/Main';
